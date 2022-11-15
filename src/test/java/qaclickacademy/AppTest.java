ab11fd5cd9490a27374387ae77bf352deebce721
gitneww <pratima.a.chittampalle@gmail.com>
2022-11-16 01:43:56 +0530

    develop commit

--- src/test/java/qaclickacademy/AppTest.java
+++ src/test/java/qaclickacademy/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        System.out.print("tuuuuuuuuuuuuuuuuuu");
+        System.out.print("develop");
     }
 
     /**
